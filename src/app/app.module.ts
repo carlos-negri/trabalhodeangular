046cc4c89294940b7ddfe6a222769382b0c95c82
Carlos <chnnegri@gmail.com>
2024-10-23 21:00:30 -0300

    sera q foi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho/cabecalho.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
