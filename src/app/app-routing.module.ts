611e91a2955e70a502f51c00bb9095b0416153bc
Carlos <chnnegri@gmail.com>
2024-10-30 20:11:02 -0300

    sera q foi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PaginaInicialComponent} from "./componentes/pagina-inicial/pagina-inicial.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "pagina-inicial", pathMatch: "full"},  //configurar primeira pagina que vai abrir
+  {path: "pagina-inicial", component: PaginaInicialComponent},
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CabecalhoComponent } from './cabecalho/cabecalho/cabecalho.component';
-import { RodapeComponent } from './rodape/rodape.component';
-import { InicioComponent } from './inicio/inicio.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { PaginaInicialComponent } from './componentes/pagina-inicial/pagina-inicial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
     RodapeComponent,
-    InicioComponent
+    InicioComponent,
+    PaginaInicialComponent
   ],
   imports: [
     BrowserModule,
